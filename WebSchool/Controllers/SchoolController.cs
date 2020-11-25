0cd1e9eb6193f65d2062573a2d30c697db1e66c8
idrizovdjem <idrizovdjem@gmail.com>
2020-11-25 21:50:23 +0200

    Create SchoolController. Add Create method and view

---
+++ WebSchool/Controllers/SchoolController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebSchool.Controllers
+{
+    public class SchoolController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
