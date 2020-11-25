5973c5e3d4e699370a0ff1e8afe6815ade647aae
idrizovdjem <idrizovdjem@gmail.com>
2020-11-25 15:54:37 +0200

    Create Payment controller and dummy VerifyPayment action and view

---
+++ WebSchool/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSchool.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult VerifyPayment()
+        {
+            return View();
+        }
+    }
+}
