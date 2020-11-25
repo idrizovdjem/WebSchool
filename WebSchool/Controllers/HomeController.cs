712fb3799e60877cb341c91b4e23017cac092683
idrizovdjem <idrizovdjem@gmail.com>
2020-11-25 12:23:08 +0200

    Change home index view

--- WebSchool/Controllers/HomeController.cs
+++ WebSchool/Controllers/HomeController.cs
@@ -5,7 +5,12 @@ namespace WebSchool.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index()
-        {
+        { 
+            if(this.User.Identity.IsAuthenticated)
+            {
+                return Redirect("/School/id=someSchoolId");
+            }
+
             return View();
         }
     }
