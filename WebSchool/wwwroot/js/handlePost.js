db4c5ac7d78f706e13321bae384d27d3821e08b8
idrizovdjem <idrizovdjem@gmail.com>
2020-11-28 11:51:46 +0200

    Add post creation form style and client-side logic

---
+++ WebSchool/wwwroot/js/handlePost.js
@@ -0,0 +1,13 @@
+﻿const postForm = document.getElementById("postForm");
+
+let isCreatePostOpened = true;
+function handlePost() {
+    if (!isCreatePostOpened) {
+        postForm.style.display = "none";
+        isCreatePostOpened = true;
+    }
+    else {
+        postForm.style.display = "block";
+        isCreatePostOpened = false;
+    }
+}
