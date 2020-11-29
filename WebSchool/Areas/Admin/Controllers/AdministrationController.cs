7f7318b98bd479ac8454cc1a750dab73ecf8a633
idrizovdjem <idrizovdjem@gmail.com>
2020-11-29 20:45:23 +0200

    Design administration panel

---
+++ WebSchool/Areas/Admin/Controllers/AdministrationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebSchool.Areas.Admin.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult Panel()
+        {
+            return View();
+        }
+    }
+}
