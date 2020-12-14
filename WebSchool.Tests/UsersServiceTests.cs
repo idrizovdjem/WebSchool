3e8db5fbe9d4f89bb97fd572970cae6b68559d38
idrizovdjem <idrizovdjem@gmail.com>
2020-12-14 22:23:55 +0200

    Add more tests for UsersService

--- WebSchool.Tests/UsersServiceTests.cs
+++ WebSchool.Tests/UsersServiceTests.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using WebSchool.Data;
 using NUnit.Framework;
-using WebSchool.Data;
 using WebSchool.Services;
+using WebSchool.Data.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebSchool.Tests
 {
@@ -18,6 +20,86 @@ namespace WebSchool.Tests
             var result = usersService.GetUserByEmail("test@mail.com");
 
             Assert.Null(result);
+        }
+
+        [Test]
+        public async Task GetUserByEmailReturnsUser()
+        {
+            var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("test");
+            var dbContext = new ApplicationDbContext(dbOptions.Options);
+
+            var user = new ApplicationUser()
+            {
+                Email = "test@mail.com"
+            };
+            await dbContext.Users.AddAsync(user);
+            await dbContext.SaveChangesAsync();
+
+            var usersService = new UsersService(dbContext, null, null);
+            var result = usersService.GetUserByEmail("test@mail.com");
+
+            Assert.That(result.Id != null);
+        }
+
+        [Test]
+        public void GetUserByIdReturnsNullWhenThereIsNoUserWithThatId()
+        {
+            var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase("test");
+            var dbContext = new ApplicationDbContext(dbOptions.Options);
+
+            var usersService = new UsersService(dbContext, null, null);
+            var result = usersService.GetUserById("some id");
+
+            Assert.Null(result);
+        }
+
+        [Test]
+        public async Task GetUserByIdReturnsUserCorrectly()
+        {
+            var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase("test");
+            var context = new ApplicationDbContext(dbOptions.Options);
+
+            var user = new ApplicationUser();
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            var usersService = new UsersService(context, null, null);
+            var result = usersService.GetUserById(user.Id);
+
+            Assert.That(result.Id != null);
+        }
+
+        [Test]
+        public void GetUserForEditReturnsNullWhenUserWithIdIsMissing()
+        {
+            var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase("test");
+            var dbContext = new ApplicationDbContext(dbOptions.Options);
+
+            var usersService = new UsersService(dbContext, null, null);
+            var result = usersService.GetUserForEdit("some id");
+
+            Assert.Null(result);
+        }
+
+        [Test]
+        public async Task GetUserForEditReturnsUserWhenThereisUserWithThatId()
+        {
+            var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase("test");
+            var context = new ApplicationDbContext(dbOptions.Options);
+
+            var user = new ApplicationUser();
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            var usersService = new UsersService(context, null, null);
+            var result = usersService.GetUserForEdit(user.Id);
+
+            Assert.That(result.Id != null);
         }
     }
 }
