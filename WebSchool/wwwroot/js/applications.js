bcf043b2315c056d60039df1d6df5b92f6de8489
idrizovdjem <idrizovdjem@gmail.com>
2021-05-30 19:02:07 +0300

    Add boostrap to applications view

--- WebSchool/wwwroot/js/applications.js
+++ WebSchool/wwwroot/js/applications.js
@@ -1,6 +1,6 @@
 ﻿Array.from(document.getElementsByClassName('role-select')).forEach(select => {
     const parentNode = select.parentElement;
-    const roleField = parentNode.nextElementSibling.children[0].children[2];
+    const roleField = parentNode.parentNode.nextElementSibling.children[0].children[2];
 
     select.addEventListener('change', (event) => {
         const role = event.target.value;
